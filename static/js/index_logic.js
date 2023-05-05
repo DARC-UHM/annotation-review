7dc99d9f8d5fd0801b33b6a2a4acf54b5a5dc3bb
Rob Godfrey <102856686+robertgodfrey@users.noreply.github.com>
2023-05-05 08:34:20 -1000

    Filter style updates

--- static/js/index_logic.js
+++ static/js/index_logic.js
@@ -56,4 +56,5 @@ autocomplete(sequenceNameInput[0], sequences);
 
 $('#plusButton').on('click', plusClick);
 sequenceNameInput.on('input', checkSequence);
-document.getElementById('index').addEventListener('click', checkSequence);
+$('#index').on('click', checkSequence);
+$('#taxonRankSelect').on('change', () => $('#taxonRankEntry').attr('placeholder', `Enter ${$('#taxonRankSelect').val().toLowerCase()}`));
