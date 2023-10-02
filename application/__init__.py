eb26f2243891b8a45b098cdf9b0add89ab896972
Rob Godfrey <102856686+robertgodfrey@users.noreply.github.com>
2023-10-02 13:02:08 -1000

    Reset stashed changes

--- application/__init__.py
+++ application/__init__.py
@@ -4,7 +4,7 @@ from threading import Timer
 
 
 def open_browser():
-    # webbrowser.open_new('http://127.0.0.1:8000')
+    webbrowser.open_new('http://127.0.0.1:8000')
     print('\n\033[1;32;48mApplication running. Press CTRL + C to stop.\033[1;37;0m\n')
 
 
