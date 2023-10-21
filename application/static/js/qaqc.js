ff74baaae5ba38caebeb5d70ce6479e2f5e210a4
Rob Godfrey <102856686+robertgodfrey@users.noreply.github.com>
2023-10-20 16:20:20 -1000

    Add new association row

--- application/static/js/qaqc.js
+++ application/static/js/qaqc.js
@@ -201,6 +201,44 @@ window.onhashchange = () => {
     updateHash();
 };
 
+function addAssociationRow() {
+    // TODO check to see if there are more associations we should add here
+    $('#editModalFields').children().last().remove(); // get rid of the add button
+    $('#editModalFields').append(`
+        <div class="row pb-2 pt-4 text-center">
+            <div class="col-4 ms-4 ps-4">
+                <select style="width: 150px;" onchange="console.log('hayyy');">
+                    <option>s1</option>
+                    <option>s2</option>
+                    <option>upon</option>
+                    <option>population-quantity</option>
+                    <option>identity-reference</option>
+                    <option>identity-certainty</option>
+                    <option>comment</option>
+                    <option>occurrence-remarks</option>
+                    <option>categorical-abundance</option>
+                </select>
+                <div class="small">New Association</div>
+            </div>
+            <div class="col-5">
+                <input type="text">
+                <div class="small">Value</div>
+            </div>
+            <div class="col-2 d-flex justify-content-end">
+                <button type="button" class="qaqcCheckButton">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" fill="currentColor" class="bi bi-check" viewBox="0 0 16 16">
+                        <path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.267.267 0 0 1 .02-.022z"/>
+                    </svg>
+                </button><button type="button" class="qaqcXButton">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16">
+                      <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" stroke="currentColor" stroke-width="0.5px"/>
+                    </svg>
+                </button>
+            </div>
+        </div>
+    `);
+}
+
 async function updateConceptName(uuid) {
     const formData = new FormData();
     formData.append('observation_uuid', uuid);
@@ -221,6 +259,18 @@ async function updateConceptName(uuid) {
     }
 }
 
+async function createAssociation() {
+
+}
+
+async function updateAssociation() {
+    
+}
+
+async function deleteAssociation() {
+
+}
+
 // get the annotation data and add it to the modal
 $(document).ready(function () {
     $('#editModal').on('show.bs.modal', function (e) {
@@ -233,7 +283,7 @@ $(document).ready(function () {
                     Concept:
                 </div>
                 <div class="col-5">
-                    <input type="text" id="editConceptName" name="concept" class="modal-text-qaqc">
+                    <input type="text" id="editConceptName" class="modal-text-qaqc">
                 </div>
                 <div class="col-2 d-flex justify-content-end">
                     <button type="button" class="qaqcCheckButton" onclick="updateConceptName('${annotation.observation_uuid}')">
@@ -261,7 +311,7 @@ $(document).ready(function () {
                         ${ass.link_name}:
                     </div>
                     <div class="col-5">
-                        <input type="text" id="${ass.link_name}-${index}" name="${ass.link_name}-${index}" class="modal-text-qaqc">
+                        <input type="text" id="${ass.link_name}-${index}" class="modal-text-qaqc">
                     </div>
                     <div class="col-2 d-flex justify-content-end">
                         <button type="button" class="qaqcCheckButton">
@@ -286,7 +336,10 @@ $(document).ready(function () {
                 autocomplete(document.getElementById(`${ass.link_name}-${index}`), allConcepts);
             }
         });
-
-        $(this).find('#editObservationUuid').val(annotation.observation_uuid);
+        $('#editModalFields').append(`
+            <div class="row my-2">
+                <button type="button" class="plusButton" onclick="addAssociationRow()">Add Association +</button>
+            </div>
+        `);
     });
 });
