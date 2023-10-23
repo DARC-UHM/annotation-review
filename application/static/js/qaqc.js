ebf1ba56148158ad9d3fbca0934885ff7d2eaf98
Rob Godfrey <102856686+robertgodfrey@users.noreply.github.com>
2023-10-23 11:53:06 -1000

    Add confirm delete modal

--- application/static/js/qaqc.js
+++ application/static/js/qaqc.js
@@ -134,35 +134,6 @@ function updateHash() {
     });
 }
 
-function updateAnnotation() {
-    event.preventDefault();
-    $('#load-overlay').removeClass('loader-bg-hidden');
-    $('#load-overlay').addClass('loader-bg');
-    $('#editModal').modal('hide');
-    const formData = new FormData($('#updateAnnotationForm')[0]);
-    fetch('/update-annotation', {
-        method: 'POST',
-        body: formData,
-    })
-        .then((result) => {
-            if (result.status === 204) {
-                const index = annotations.findIndex((anno) => anno.observation_uuid === formData.get('observation_uuid'));
-                for (const pair of formData.entries()){
-                    annotations[index][pair[0]] = pair[1];
-                }
-                updateFlashMessages('Annotation successfully updated', 'success');
-                updateHash();
-            } else if (result.status === 304) {
-                updateFlashMessages('No changes made', 'secondary');
-            } else {
-                updateFlashMessages('Failed to update annotation - please try again', 'danger');
-            }
-            $('#load-overlay').addClass('loader-bg-hidden');
-            $('#load-overlay').removeClass('loader-bg');
-        })
-        .catch((err) => console.log(err));
-}
-
 document.addEventListener('DOMContentLoaded', function(event) {
     const url = new URL(window.location.href);
     let vesselName;
@@ -346,11 +317,10 @@ async function updateAssociation(uuid, link_name, textInputId) {
     $(`#button${textInputId}`).attr('disabled', true);
 }
 
-async function deleteAssociation() {
-
+async function deleteAssociation(associationToDelete) {
+    console.log(associationToDelete)
 }
 
-// get the annotation data and add it to the modal
 $(document).ready(function () {
     $('#editModal').on('show.bs.modal', function (e) {
         const annotation = $(e.relatedTarget).data('anno');
@@ -393,7 +363,7 @@ $(document).ready(function () {
                             <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" fill="currentColor" class="bi bi-check" viewBox="0 0 16 16">
                                 <path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.267.267 0 0 1 .02-.022z"/>
                             </svg>
-                        </button><button type="button" class="qaqcXButton">
+                        </button><button type="button" class="qaqcXButton" data-bs-toggle="modal" data-ass='${ JSON.stringify(ass) }' data-bs-target="#deleteAssociationModal">
                             <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16">
                               <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" stroke="currentColor" stroke-width="0.5px"/>
                             </svg>
@@ -419,5 +389,9 @@ $(document).ready(function () {
                 <button type="button" class="plusButton" onclick="addAssociationRow('${annotation.observation_uuid}')">Add Association +</button>
             </div>
         `);
+    });
+
+    $('#deleteAssociationModal').on('show.bs.modal', function (e) {
+        $('#deleteAssociationButton').on('click', deleteAssociation($(e.relatedTarget).data('ass')));
     });
 });
