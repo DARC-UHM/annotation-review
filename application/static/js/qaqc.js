9fa867d77afe4746e1f8677273e0f302dd2f2bed
Rob Godfrey <102856686+robertgodfrey@users.noreply.github.com>
2023-10-23 14:21:42 -1000

    Update modal on association update

--- application/static/js/qaqc.js
+++ application/static/js/qaqc.js
@@ -245,6 +245,7 @@ function cancelAddAssociation() {
 }
 
 async function updateConceptName(uuid) {
+    const annoIndex = annotations.findIndex((anno) => anno.observation_uuid === workingAnnotationUuid);
     const formData = new FormData();
     formData.append('observation_uuid', uuid);
     formData.append('concept', $('#editConceptName').val());
@@ -259,6 +260,9 @@ async function updateConceptName(uuid) {
     });
     if (res.status === 204) {
         updateFlashMessages('Successfully updated concept name', 'success');
+        annotations[annoIndex].concept = $('#editConceptName').val();
+        $('#qaqcUpdateConceptButton').attr('disabled', true);
+        updateHash();
     } else {
         updateFlashMessages('Failed to update concept name', 'danger');
     }
@@ -302,11 +306,10 @@ async function createAssociation(observation_uuid) {
             });
         }
         updateHash();
+        loadModal(annotations[annoIndex]);
     } else {
         updateFlashMessages(`Failed to add association: ${res.status}`, 'danger');
     }
-
-    // todo update modal
 }
 
 async function updateAssociation(uuid, link_name, textInputId) {
@@ -351,83 +354,85 @@ async function deleteAssociation() {
         updateFlashMessages('Successfully deleted association', 'success');
         annotations[annoIndex].associations.splice(assIndex, 1);
         updateHash();
+        loadModal(annotations[annoIndex]);
     } else {
         updateFlashMessages(`Failed to delete association: ${res.status}`, 'danger');
     }
     $('#deleteAssociationModal').modal('hide');
-
-    // todo update modal
 }
 
-$(document).ready(function () {
-    $('#editModal').on('show.bs.modal', (e) => {
-        const annotation = $(e.relatedTarget).data('anno');
-        const sortedAssociations = annotation.associations.sort((a, b) => (a.link_name > b.link_name) ? 1 : ((b.link_name > a.link_name) ? -1 : 0));
-        workingAnnotationUuid = annotation.observation_uuid;
-        $('#editModalFields').empty();
+function loadModal(annotationData) {
+    const sortedAssociations = annotationData.associations.sort((a, b) => (a.link_name > b.link_name) ? 1 : ((b.link_name > a.link_name) ? -1 : 0));
+    workingAnnotationUuid = annotationData.observation_uuid;
+    $('#editModalFields').empty();
+    $('#editModalFields').append(`
+        <div class="row pb-2">
+            <div class="col-4 ms-4 ps-4 my-auto modal-label">
+                Concept:
+            </div>
+            <div class="col-5">
+                <input type="text" id="editConceptName" class="modal-text-qaqc">
+            </div>
+            <div class="col-2 d-flex justify-content-center pe-3">
+                <button id="qaqcUpdateConceptButton" type="button" class="qaqcCheckButton" onclick="updateConceptName('${annotationData.observation_uuid}')" disabled>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" fill="currentColor" class="bi bi-check" viewBox="0 0 16 16">
+                        <path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.267.267 0 0 1 .02-.022z"/>
+                    </svg>
+                </button>
+            </div>
+        </div>
+    `);
+    const conceptNameField = $('#editConceptName');
+    autocomplete(conceptNameField, allConcepts);
+    conceptNameField.val(annotationData.concept);
+    conceptNameField.on('input', () => validateName(conceptNameField.val(), $('#qaqcUpdateConceptButton')));
+    conceptNameField.on('change', () => validateName(conceptNameField.val(), $('#qaqcUpdateConceptButton')));
+
+    sortedAssociations.forEach((ass, index) => {
         $('#editModalFields').append(`
             <div class="row pb-2">
                 <div class="col-4 ms-4 ps-4 my-auto modal-label">
-                    Concept:
+                    ${ass.link_name}:
                 </div>
                 <div class="col-5">
-                    <input type="text" id="editConceptName" class="modal-text-qaqc">
+                    <input type="text" id="${ass.link_name}-${index}" class="modal-text-qaqc">
                 </div>
-                <div class="col-2 d-flex justify-content-center pe-3">
-                    <button id="qaqcUpdateConceptButton" type="button" class="qaqcCheckButton" onclick="updateConceptName('${annotation.observation_uuid}')" disabled>
+                <div class="col-2 d-flex justify-content-end">
+                    <button id="button${ass.link_name}-${index}" type="button" class="qaqcCheckButton" onclick="updateAssociation('${ass.uuid}', '${ass.link_name}', '${ass.link_name}-${index}')" disabled>
                         <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" fill="currentColor" class="bi bi-check" viewBox="0 0 16 16">
                             <path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.267.267 0 0 1 .02-.022z"/>
+                        </svg>
+                    </button><button type="button" class="qaqcXButton" data-bs-toggle="modal" data-ass='${ JSON.stringify(ass) }' data-bs-target="#deleteAssociationModal">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16">
+                          <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" stroke="currentColor" stroke-width="0.5px"/>
                         </svg>
                     </button>
                 </div>
             </div>
         `);
-        const conceptNameField = $(this).find('#editConceptName');
-        autocomplete($('#editConceptName'), allConcepts);
-        conceptNameField.val(annotation.concept);
-        conceptNameField.on('input', () => validateName(conceptNameField.val(), $('#qaqcUpdateConceptButton')));
-        conceptNameField.on('change', () => validateName(conceptNameField.val(), $('#qaqcUpdateConceptButton')));
+        const field = $(`#${ass.link_name}-${index}`);
+        const button = $(`#button${ass.link_name}-${index}`);
+        if (toConcepts.includes(ass.link_name)) {
+            field.val(ass.to_concept);
+            field.on('input', () => validateName(field.val(), button));
+            field.on('change', () => validateName(field.val(), button));
+            autocomplete($(`#${ass.link_name}-${index}`), allConcepts);
+        } else {
+            field.val(ass.link_value);
+            field.on('input', () => button.prop('disabled', field.val() <= 0));
+            field.on('change', () => button.prop('disabled', field.val() <= 0));
+        }
+    });
+    $('#editModalFields').append(`
+        <div class="row my-3">
+            <button type="button" class="plusButton" onclick="addAssociationRow('${annotationData.observation_uuid}')">Add Association +</button>
+        </div>
+    `);
+}
 
-        sortedAssociations.forEach((ass, index) => {
-            $('#editModalFields').append(`
-                <div class="row pb-2">
-                    <div class="col-4 ms-4 ps-4 my-auto modal-label">
-                        ${ass.link_name}:
-                    </div>
-                    <div class="col-5">
-                        <input type="text" id="${ass.link_name}-${index}" class="modal-text-qaqc">
-                    </div>
-                    <div class="col-2 d-flex justify-content-end">
-                        <button id="button${ass.link_name}-${index}" type="button" class="qaqcCheckButton" onclick="updateAssociation('${ass.uuid}', '${ass.link_name}', '${ass.link_name}-${index}')" disabled>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" fill="currentColor" class="bi bi-check" viewBox="0 0 16 16">
-                                <path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.267.267 0 0 1 .02-.022z"/>
-                            </svg>
-                        </button><button type="button" class="qaqcXButton" data-bs-toggle="modal" data-ass='${ JSON.stringify(ass) }' data-bs-target="#deleteAssociationModal">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16">
-                              <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" stroke="currentColor" stroke-width="0.5px"/>
-                            </svg>
-                        </button>
-                    </div>
-                </div>
-            `);
-            const field = $(`#${ass.link_name}-${index}`);
-            const button = $(`#button${ass.link_name}-${index}`);
-            if (toConcepts.includes(ass.link_name)) {
-                field.val(ass.to_concept);
-                field.on('input', () => validateName(field.val(), button));
-                field.on('change', () => validateName(field.val(), button));
-                autocomplete($(`#${ass.link_name}-${index}`), allConcepts);
-            } else {
-                field.val(ass.link_value);
-                field.on('input', () => button.prop('disabled', field.val() <= 0));
-                field.on('change', () => button.prop('disabled', field.val() <= 0));
-            }
-        });
-        $('#editModalFields').append(`
-            <div class="row my-3">
-                <button type="button" class="plusButton" onclick="addAssociationRow('${annotation.observation_uuid}')">Add Association +</button>
-            </div>
-        `);
+$(document).ready(function () {
+    $('#editModal').on('show.bs.modal', (e) => {
+        loadModal($(e.relatedTarget).data('anno'));
     });
 
     $('#deleteAssociationModal').on('show.bs.modal', (e) => {
