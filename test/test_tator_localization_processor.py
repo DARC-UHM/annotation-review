e206b01b7cc2bc9225fe0e9639fc5feca2d4f033
Rob Godfrey <102856686+robertgodfrey@users.noreply.github.com>
2024-05-30 12:36:54 -1000

    Remove temp tests

--- test/test_tator_localization_processor.py
+++
@@ -1,4 +0,0 @@
-class TestLocalizationProcessor:
-    def test_tests_done(self):
-        # todo
-        assert False
--- test/test_tator_qaqc_processor.py
+++
@@ -1,4 +0,0 @@
-class TestTatorQaqcProcessor:
-    def test_tests_done(self):
-        # todo
-        assert False
