d021644b573511a13b6f7e94b8b8308485f3a98e
Rob Godfrey <rob_godfrey@outlook.com>
2025-02-20 19:11:51 -0800

    Add Georgia to known reviewers

--- application/server/constants.py
+++ application/server/constants.py
@@ -1,10 +1,12 @@
 KNOWN_ANNOTATORS = {
+    # Tator ID: Name
     22: 'Jeff Drazen',
     24: 'Meagan Putts',
     25: 'Sarah Bingo',
     332: 'Nikki Cunanan',
     433: 'Aaron Judah',
     491: 'Harold Carlson',
+    663: 'Georgia Watson',
 }
 TERM_RED = '\033[1;31;48m'
 TERM_YELLOW = '\033[1;93m'
