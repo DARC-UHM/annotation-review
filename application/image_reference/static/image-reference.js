6675729772ca113b858c59fb8dfbd03ea5fd9f98
Rob Godfrey <rob_godfrey@outlook.com>
2025-04-20 16:56:35 -0700

    Add filter UI

--- application/image_reference/static/image-reference.js
+++ application/image_reference/static/image-reference.js
@@ -1,12 +1,29 @@
+const taxonRanks = ['phylum', 'class', 'order', 'family', 'genus', 'species'];
+const phyla = {};
+
 let filteredImageReferences = imageReferences;
 let filter = {};
+
+$(document).ready(() => {
+    populatePhyla();
+    console.log(phyla);
+    getFiltersFromHash();
+    updatePhylogenyFilterSelects();
+    updateImageGrid();
+});
+
+window.onhashchange = () => {
+    getFiltersFromHash();
+    updatePhylogenyFilterSelects();
+    updateImageGrid();
+};
 
 function updateImageGrid() {
     $('#imageGrid').empty();
     filteredImageReferences = imageReferences;
     for (const key of Object.keys(filter)) {
         filteredImageReferences = filteredImageReferences.filter((imageRef) => {
-            return imageRef[key]?.toLowerCase().includes(filter[key].toLowerCase());
+            return imageRef[key === 'class' ? 'class_name' : key]?.toLowerCase().includes(filter[key].toLowerCase());
         });
     }
     filteredImageReferences.forEach((imageRef) => {
@@ -45,16 +62,99 @@ function getFiltersFromHash() {
         return;
     }
     for (const hashPair of hash.split('&')) {
-        filter[hashPair.split('=')[0]] = hashPair.split('=')[1];
+        filter[hashPair.split('=')[0]] = hashPair.split('=')[1].replaceAll('%20', ' ');
     }
 }
 
-$(document).ready(() => {
-    getFiltersFromHash();
-    updateImageGrid();
-});
+function populatePhyla() {
+    // populate available phylogeny (probs should just save this info on the backend)
+    imageReferences.forEach((imageRef) => {
+        if (!phyla[imageRef.phylum]) {
+            phyla[imageRef.phylum] = {};
+        }
+        if (!phyla[imageRef.phylum][imageRef.class_name]) {
+            phyla[imageRef.phylum][imageRef.class_name] = {};
+        }
+        if (!phyla[imageRef.phylum][imageRef.class_name][imageRef.order]) {
+            phyla[imageRef.phylum][imageRef.class_name][imageRef.order] = {};
+        }
+        if (!phyla[imageRef.phylum][imageRef.class_name][imageRef.order][imageRef.family]) {
+            phyla[imageRef.phylum][imageRef.class_name][imageRef.order][imageRef.family] = {};
+        }
+        if (!phyla[imageRef.phylum][imageRef.class_name][imageRef.order][imageRef.family][imageRef.genus]) {
+            phyla[imageRef.phylum][imageRef.class_name][imageRef.order][imageRef.family][imageRef.genus] = {};
+        }
+    });
+}
 
-window.onhashchange = () => {
-    getFiltersFromHash();
-    updateImageGrid();
-};
+function addPhylogenyFilterSelect(taxonRank, selectedValue) {
+    let rankOptions;
+    switch (taxonRank) {
+        case 'phylum':
+            rankOptions = (Object.keys(phyla));
+            break;
+        case 'class':
+            rankOptions = (Object.keys(phyla[filter.phylum]));
+            break;
+        case 'order':
+            rankOptions = (Object.keys(phyla[filter.phylum][filter.class]));
+            break;
+        case 'family':
+            rankOptions = (Object.keys(phyla[filter.phylum][filter.class][filter.order]));
+            break;
+        case 'genus':
+            rankOptions = (Object.keys(phyla[filter.phylum][filter.class][filter.order][filter.family]));
+            break;
+    }
+    $('#filterList').append(`
+        <span id="${taxonRank}Filter" class="small">
+            <span class="position-relative">
+                <select id="${taxonRank}FilterSelect" onchange="updatePhylogenyFilter('${taxonRank}')" style="background: #2d3541;">
+                    <option value="all">Any ${taxonRank}</option>
+                    ${rankOptions.map((option) => 
+                        `<option value="${option}" ${selectedValue === option ? 'selected' : ''}>${option}</option>`
+                    )}
+                </select>
+                <span class="position-absolute dropdown-chev">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-down" viewBox="0 0 16 16">
+                      <path fill-rule="evenodd" d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"/>
+                    </svg>
+                </span>
+            </span>
+        </span>
+    `);
+}
+
+function updatePhylogenyFilter(taxonRank) {
+    const selected = $(`#${taxonRank}FilterSelect`).val();
+    if (selected === 'all') {
+        delete filter[taxonRank];
+    } else {
+        filter[taxonRank] = selected;
+    }
+    // remove all lower ranks
+    for (let i = taxonRanks.indexOf(taxonRank) + 1; i < taxonRanks.length; i++) {
+        delete filter[taxonRanks[i]];
+    }
+    location.hash = Object.keys(filter)
+        .map((key) => `${key}=${filter[key]}`)
+        .join('&');
+}
+
+window.updatePhylogenyFilter = updatePhylogenyFilter;
+
+function updatePhylogenyFilterSelects() {
+    $('#filterList').empty();
+    for (const filterName of Object.keys(filter)) {
+        addPhylogenyFilterSelect(filterName, filter[filterName]);
+        if (filterName !== 'genus') {
+            $('#filterList').append('→');
+        }
+    }
+    for (const taxonRank of taxonRanks) {
+        if (!filter[taxonRank]) {
+            addPhylogenyFilterSelect(taxonRank, '');
+            break;
+        }
+    }
+}
