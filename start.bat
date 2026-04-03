68c71840d1902f46a75366295e769119dcf91686
Rob Godfrey <rob_godfrey@outlook.com>
2026-04-02 21:57:04 -0700

    Update waitress start command

--- start.bat
+++ start.bat
@@ -1,4 +1,4 @@
 @echo off
 git pull
 start /b cmd /c "timeout /t 2 /nobreak >nul && start http://localhost:8000"
-waitress-serve --threads=3 --port=8000 --call application:create_app
+python -m waitress --threads=3 --port=8000 --call application:create_app
